1869b7126ae1981181db62c777d3cc64adc0e6e5
JR - 11EG 719149 - <120673633+JawadRiz@users.noreply.github.com>
2022-12-22 17:25:04 +0000

    Created the graph for the calculation

--- main.py
+++ main.py
@@ -1,23 +1,102 @@
 import math
+import os
+import pygame
+import random
+from pygame.locals import *
+
+SCREENWIDTH  = 612
+SCREENHEIGHT = 512
+
+IMAGES, BACKGROUNDS, SPRITES = {}, {}, {}
+
+SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+pygame.init()
+
+#Alternates between dots to reduce lag
+tableOfDots = []
+IMAGES['dot'] = pygame.image.load('assets/dot.png').convert_alpha()
+IMAGES['target'] = pygame.image.load('assets/target.png').convert_alpha()
 
 equation = input("type equation: f(x) = ")
 
+#  Sets up the level by placing the target, obstructions, etc.
+def SetupLevel():
+  #  Place target
+  
+
+
+#  Purpose is to find subfunctions within the equation (ex. find sin, cos, log, etc.)
+def FindFunction(functionName, equation):
+
+  startingVal = 0
+  while True:
+    try:
+      FunctionLocation = equation.find(functionName, startingVal)
+      if FunctionLocation >= 0 and FunctionLocation < len(equation):
+        numberLocation = FunctionLocation + len(functionName)
+
+        #  Counts the amount of brackets within the function. If it drops below 0, then that means the function has ended
+        BracketCounter = 0
+        numberLocationEnd = FunctionLocation
+        for letter in range(len(equation)):
+          if letter >= FunctionLocation:
+            if equation[letter] == "(":
+              BracketCounter += 1
+            elif equation[letter] == ")":
+              BracketCounter -= 1
+              if BracketCounter == 0:
+                numberLocationEnd = letter
+                break
+            
+        functionEquation = (equation[FunctionLocation:(numberLocationEnd + 1)])
+        innerEquation = (equation[numberLocation:numberLocationEnd])
+        
+        #  Clean up/calculate equations and functions within the function (ex. sin(sin(pi/6)) would simplify to sin(0.5) instead)
+        #  Basically just recalling the same function to calculate the subfunction/equation
+        innerEquationValue = eval(CleanupEquation(innerEquation))
+
+        if functionName == "sin(":
+          innerEquationValue = math.sin(innerEquationValue)
+        elif functionName == "cos(":
+          innerEquationValue = math.cos(innerEquationValue)
+        elif functionName == "ln(":
+          innerEquationValue = math.log(innerEquationValue, math.e)
+        elif functionName == "sqrt(":
+          innerEquationValue = math.sqrt(innerEquationValue)
+
+        equation = equation.replace(functionEquation, str(innerEquationValue))
+      else:
+        break
+      startingVal = numberLocationEnd
+    except:
+      break
+  return equation
+  
 #  Since eval has different syntax for operations, we have to make the expression compatible
 #  Turns x^2 to x**2, and 3(x)2  to 3*x*2 etc.
 def CleanupEquation(equation):
   #  Change ^ to **
-  if equation.find("^") > 0:
+  if equation.find("^") >= 0:
     equation = equation.replace("^", "**")
 
   #  Change e to the actual value
-  if equation.find("e") > 0:
+  if equation.find("e") >= 0:
     equation = equation.replace("e", str(math.e))
 
   # Change pi to to the actual value
-  if equation.find("pi") > 0:
+  if equation.find("pi") >= 0:
     equation = equation.replace("pi", str(math.pi))
 
-    
+  # Change log to natural log automatically
+  if equation.find("log") >= 0:
+    equation = equation.replace("log", "ln")
+
+  # Look for sin, cos, ln, and sqrt function
+  equation = FindFunction("sin(", equation)
+  equation = FindFunction("cos(", equation)
+  equation = FindFunction("ln(", equation)
+  equation = FindFunction("sqrt(", equation)
+  
   # Look for multiplications via opening brackets
   # Basically loops through all the brackets and sees if its directly next to a real number
   startingVal = 1
@@ -32,7 +111,7 @@ def CleanupEquation(equation):
         equation = ''.join((equation[:openingBracketLocation],'*', equation[openingBracketLocation:]))
         openingBracketLocation += 1
       except:
-        print(" ")
+        openingBracketLocation = openingBracketLocation
     else:
       break
     startingVal = openingBracketLocation + 1
@@ -40,6 +119,7 @@ def CleanupEquation(equation):
   # Look for multiplications via closing brackets
   # Basically same code as above but with closing brackets
   startingVal = 1
+  
   while True:
     closingBracketLocation = equation.find(")", startingVal)
     if closingBracketLocation >= 1 and closingBracketLocation < len(equation):
@@ -50,44 +130,49 @@ def CleanupEquation(equation):
         equation = ''.join((equation[:numberLocation],'*', equation[numberLocation:]))
         closingBracketLocation += 1
       except:
-        print(" ")
+        closingBracketLocation = closingBracketLocation
     else:
       break
     startingVal = closingBracketLocation + 1
-
-  # Look for sin function
-  startingVal = 0
-  while True:
-    #try:
-      sinFunctionLocation = equation.find("sin(", startingVal)
-      if sinFunctionLocation >= 0 and sinFunctionLocation < len(equation):
-        numberLocation = sinFunctionLocation + 5
-        numberLocationEnd = equation.find(")", sinFunctionLocation)
-        #  Clean up the equation within the sin function (ex. sin(2(2)) would simplify to sin(4) instead)
-        innerEquation = (equation[numberLocation:numberLocationEnd])
-        print(equation, innerEquation)
-        innerEquationValue = eval(CleanupEquation(innerEquation))
-        innerEquationValue = math.sin(innerEquationValue)
-        equation.replace(str(innerEquation), str(innerEquationValue))
-        print(equation)
-      else:
-        break
-      startingVal = numberLocationEnd
-   # except:
-     # break
-            
-          
+  os.system('clear')
   return equation
     
 
-def CalculateEquation(equation):
-  num = 0
-  while num < 10:
-      newEquation = equation.replace("x", "({})".format(str(num)))
+def CalculateEquation(equation, dotTable):
+  x = 0
+  errornum = 0
+  coordinatesTable = []
+  while x <= 28.5:
+    try:
+      newEquation = equation.replace("x", "({})".format(str(x)))
       newEquation = CleanupEquation(newEquation)
-      print((newEquation), "=")
-      num += 0.1
-
+      #print("f({})".format(str(x)), "=", (newEquation), "=", eval(newEquation))
+      coords = [x, eval(newEquation)]
+      coordinatesTable.append(coords)
+      PlotGraph(coords, dotTable)
+    except:
+      #  Might be an asymptote or undefined (x/0)
+      errornum += 1
+      coordinatesTable.append((x, "None"))
+      print("Unknown Value", errornum)
+      #  If error happens more than 15 times then theres a problem
+      if errornum > 20:
+        return "Error"
+    x += 0.1
+  return coordinatesTable
 #  Place brackets between all x to easily work with the equation
 #equation = CleanupEquation(equation.replace("x", "(x)"))
-CalculateEquation(equation)
+  
+
+def PlotGraph(coords, dotTable):
+  #Used to alternate positions between dots to reduce lag
+  SCREEN.fill((255,255,255))
+  if coords[1] != 'None':
+    pos = (100 + coords[0]*20, 200 - coords[1]*20)
+    dotTable.append([IMAGES['dot'], pos])
+    if len(dotTable) > 30:
+      dotTable.pop(0)
+  for dot in dotTable:
+    SCREEN.blit(dot[0], dot[1])
+  pygame.display.update()
+coordinates = CalculateEquation(equation, tableOfDots)
